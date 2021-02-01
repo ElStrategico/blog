eb0f8dbf1d45e6b0101ba7678b784b6c215d751b
El Strategico <strategico.dev@gmail.com>
2021-02-02 04:16:48 +0800

    Added logic for work with PostsList

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PostSearchFilter />
+    <PostsList />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostsList from "@/components/PostsList.vue";
+import PostSearchFilter from "@/components/PostSearchFilter.vue";
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {PostsList, PostSearchFilter}
 }
 </script>
