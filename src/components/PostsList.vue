96f4a97d8fe50a1503b62de4e0d520725930599d
El Strategico <strategico.dev@gmail.com>
2021-02-02 04:15:43 +0800

    Added PostsList component

---
+++ src/components/PostsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div>
+      <Loader v-if="loader"/>
+    </div>
+    <PostItem
+      v-for="post in allPosts"
+      :key="post.id"
+      :title="post.title"
+      :body="post.body"
+    />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Loader from "@/components/Loader.vue";
+import PostItem from "@/components/PostItem.vue";
+
+export default {
+  name: "PostsList",
+  components: { PostItem, Loader },
+  computed: mapGetters(["allPosts", "loader"]),
+  mounted() {
+    this.$store.dispatch("fetchPosts");
+  },
+};
+</script>
