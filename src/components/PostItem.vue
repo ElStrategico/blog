c1986ac88a228b71f9d011deac8882f02f82f0d5
El Strategico <strategico.dev@gmail.com>
2021-02-02 04:15:11 +0800

    Added PostItem component

---
+++ src/components/PostItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row post">
+    <div class="col-lg-12">
+      <h3>{{title}}</h3>
+      <p>{{body}}</p>
+      <a class="btn btn-success">Подробнее</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PostItem',
+    props: {
+        title: String,
+        body: String
+    }
+}
+</script>
+
+<style scope>
+.post {
+    background: #f2f2f2;
+    margin: 10px;
+    padding: 10px;
+}
+</style>
